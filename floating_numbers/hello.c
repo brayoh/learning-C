9a6ccb61a7bfac036cd1da6bbee172eafde268f9
Brian Njenga <bkuriakb@gmail.com>
2025-01-29 09:31:39 +0300

    Initial commit

---
+++ floating_numbers/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  float value = 0.1
+  printf("value: %0.10f\n", value);
+  
+   return 0;
+}
---
+++ floating_numbers/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float value = 0.1;
+    printf("value: %0.10f\n", value);
+    return 0;
+}
+
+
+
